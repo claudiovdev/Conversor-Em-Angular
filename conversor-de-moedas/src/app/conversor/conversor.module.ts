7de88f40d52ab1952eff2fec2716696c791b20e4
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2021-06-23 21:02:44 -0300

    Criando os components do conversor

--- conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConversorComponent } from './components/conversor.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ConversorComponent
+  ],
   imports: [
     CommonModule
   ]
